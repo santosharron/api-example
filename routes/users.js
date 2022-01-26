8468422754b6647c990abcb703b1be33a2d0cf74
Santosh Arron <santosharron2906@gmail.com>
2022-01-26 13:48:09 +0530

    initial-commit

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var users = require('../data/users');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json({ users });
+});
+
+module.exports = router;
